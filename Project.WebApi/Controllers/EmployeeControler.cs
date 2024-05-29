f26f709922b0b9db07460f760a400ca7139dd040
lukalovric <lulovric@gmail.com>
2024-05-29 11:07:00 +0200

    Added Classes

---
+++ Project.WebApi/Controllers/EmployeeControler.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeControler : ControllerBase
+    {
+    }
+}
---
+++ Project.WebApi/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project.WebApi
+{
+    public class Employee
+    {
+        public string EmployeeID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Position { get; set; }
+        public double? Salary { get; set; }
+    }
+}
